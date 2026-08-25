b7254fa4259722bf634ddfa48e4f77cde5d9c4f7
harshito0 <harshitsingh190622@gmail.com>
2026-08-25 13:29:52 +0530

    fix1

--- components/dashboard-layout.tsx
+++ components/dashboard-layout.tsx
@@ -1,19 +1,17 @@
+'use client';
+
 import React from 'react';
 import { Sidebar } from './sidebar';
-import { Topbar } from './topbar';
 
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-slate-50">
       <Sidebar />
-      <div className="flex-1 flex flex-col min-w-0">
-        <Topbar />
-        <main className="flex-1 overflow-y-auto">
-          <div className="max-w-7xl mx-auto p-6 md:p-8">
-            {children}
-          </div>
-        </main>
-      </div>
+      <main className="flex-1 min-w-0 overflow-y-auto">
+        <div className="max-w-7xl mx-auto p-6 md:p-8">
+          {children}
+        </div>
+      </main>
     </div>
   );
 }
